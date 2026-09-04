190d611576f90e153aa037397bed991385f07e39
rei-yan <holidaynamanny527@gmail.com>
2026-09-04 19:46:12 -0400

    Proof: include recursive divided-difference observer in root replay

--- Rei_omega_proof.lean
+++ Rei_omega_proof.lean
@@ -5,6 +5,7 @@ import Rei_omega_proof.TransformObserver
 import Rei_omega_proof.DynamicsGeometryObserver
 import Rei_omega_proof.BoundaryPropagationObserver
 import Rei_omega_proof.CompactCoreObserver
+import Rei_omega_proof.RecursiveDifferenceObserver
 import Rei_omega_proof.ClosureSyncObserver
 import Rei_omega_proof.AutoUpdateObserver
 
